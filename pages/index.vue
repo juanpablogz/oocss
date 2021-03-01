c8c72431d132f6e855266cd7d0149dd2e1e98228
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-02-28 19:08:29 -0500

    oocss

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,22 @@
 <template>
-  <!-- bloques,elementos, modificares -->
-  <!-- bloques componentes de nuestro sitio que queremos reutizar en otra parte -->
-  <!-- elementos forman parte del bloque y no significan nada por si solos -->
-  <!-- podemos usar bloques dentro de bloques -->
 	<div class="container">
-		<div class="card">   <!-- bloque -->
-      <a href="" class="card__thumbnail"> <!-- elemento de la card -->
-         <img class="card__img" src="@/assets/img/1.jpg" alt="">
+		<div class="card"> 
+      <a href="" class="cardThumbnail">
+         <img class="cardImg" src="@/assets/img/1.jpg" alt="">
       </a>
       <div class="card__text">
-          <h3 class="card__title">Lorem ipsum dolor, sit amet consectetur </h3>
-          <a href="#" class="card__link">Lorem ipsum dolor, sit amet consectetur </a>
-          <p class="card__content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, excepturi! Sed maxime accusantium harum hic aliquid dolorum nostrum quasi asperiores quas officiis illo impedit blanditiis dolores vel, corrupti pariatur dicta.</p>
+          <h3 class="cardTitle">Lorem ipsum dolor, sit amet consectetur </h3>
+          <a href="#" class="cardLink">Lorem ipsum dolor, sit amet consectetur </a>
+          <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, excepturi! Sed maxime accusantium harum hic aliquid dolorum nostrum quasi asperiores quas officiis illo impedit blanditiis dolores vel, corrupti pariatur dicta.</p>
           
-          <a href="" role="button" class="button button--absolute"> <!-- nuevo bloque bloque  y modificador -->
+          <a href="" role="button" class="button buttonAbsolute">
             <span>Ver Más</span> 
             <i class="button_icon fas fa-arrow-right"></i>
           </a>
       </div>
     </div>
-    <div class="frase"> 
-      <h2 class="frase__texto">lorem ipsum dolor lorem</h2>
+    <div> 
+      <h2 >lorem ipsum dolor lorem</h2>
       <a href="" class="button" role="button">
         <span>Comprar</span>
         <i class="button_icon fas fa-shopping-cart"></i>
